1114c9a59fa43fe0fc9632781e856fb8abcbafc4
CEntertain <TimGeru@gmx.de>
2025-10-22 16:48:33 +0200

    Added true scale image printing

--- GameOfLife/Exec/Utilities/PrintImage.cs
+++ GameOfLife/Exec/Utilities/PrintImage.cs
@@ -2,6 +2,7 @@
 {
     internal static class PrintImage
     {
+        private static char[] joinedThresholdChars = { '█', '▄', '▀', ' ' };
         private static string[] thresholdChars = { "██", "  " };
 
         public static void VolumeAbove(ImageManager imageManager, Image image, float threshold = .5f, bool belowInstead = false)
@@ -18,15 +19,38 @@
 
         private static void PrintBoolArray(bool[,] boolArray)
         {
-            for (int y = 0; y < boolArray.GetLength(1); y++)
+            int xScale = boolArray.GetLength(0);
+            int yScale = boolArray.GetLength(1);
+            int halvedYScale = (int)Math.Ceiling(yScale / 2f);
+            (bool, bool)[,] verticalBools = new (bool, bool)[xScale, halvedYScale];
+            JoinVerticalBools(xScale, yScale, ref verticalBools, boolArray);
+            for (int y = 0; y < halvedYScale; y++)
             {
-                for (int x = 0; x < boolArray.GetLength(0); x++)
-                    PrintThresholdChar(boolArray[x, y]);
+                for (int x = 0; x < xScale; x++)
+                    PrintJoinedThresholdChar(verticalBools[x, y]);
                 Console.WriteLine();
             }
         }
 
-        private static void PrintThresholdChar(bool boolean)
-            => Console.Write($"{(boolean ? thresholdChars[0] : thresholdChars[1])}");
+        private static void JoinVerticalBools(int xScale, int yScale, ref (bool, bool)[,] verticalBools, bool[,] boolArray)
+        {
+            for (int x = 0; x < xScale; x++)
+                for (int y = 0; y < yScale; y++)
+                    if (y % 2 == 0)
+                        verticalBools[x, y / 2].Item1 = boolArray[x, y];
+                    else
+                        verticalBools[x, y / 2].Item2 = boolArray[x, y];
+        }
+
+        private static void PrintThresholdChar(bool threshold)
+            => Console.Write(threshold ? thresholdChars[0] : thresholdChars[1]);
+        private static void PrintJoinedThresholdChar((bool, bool) verticalBoolean)
+        {
+            var (top, bottom) = verticalBoolean;
+            if (top && bottom) Console.Write(joinedThresholdChars[0]);
+            else if (!top && bottom) Console.Write(joinedThresholdChars[1]);
+            else if (top && !bottom) Console.Write(joinedThresholdChars[2]);
+            else Console.Write(joinedThresholdChars[3]);
+        }
     }
 }
