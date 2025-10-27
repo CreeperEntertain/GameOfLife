a4b85b6300818a01680cd46a53b3e70ff3964997
CEntertain <TimGeru@gmx.de>
2025-10-27 15:15:17 +0100

    HELPFOR command implemented

--- GameOfLife/Exec/Utilities/IO/Commands/HelpForCommand.cs
+++ GameOfLife/Exec/Utilities/IO/Commands/HelpForCommand.cs
@@ -6,7 +6,21 @@ namespace GameOfLife.Exec.Utilities.IO.Commands
     {
         public static void Exec()
         {
-            string command = CommandDictionary.UserInput;
+            string[] input = CommandDictionary.UserInput.Split(' ');
+            if (input.Length > 2)
+                TextOut.WriteLine("More than 2 words provided.", ConsoleColor.Red);
+            if (CommandDictionary.commands.ContainsKey(input[1]))
+                HelpFor(input[1].ToLower());
+            else
+            {
+                TextOut.Write("Command [", ConsoleColor.Red);
+                TextOut.Write(input[1].ToLower(), ConsoleColor.Yellow);
+                TextOut.WriteLine("] does not exist.", ConsoleColor.Red);
+            }
+        }
+
+        private static void HelpFor(string command)
+        {
             TextOut.Write($"Help for [", ConsoleColor.Blue);
             TextOut.Write(command, ConsoleColor.Yellow);
             TextOut.WriteLine("]:", ConsoleColor.Blue);
